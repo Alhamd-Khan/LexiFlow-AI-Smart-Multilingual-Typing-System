fe21ad72d1f956c0f9aab5859b60633bc02b8713
Alhamd <alhamdkhan016@gmail>
2026-04-02 00:08:36 +0530

    email error

--- backend/src/utils/mailer.js
+++ backend/src/utils/mailer.js
@@ -4,13 +4,12 @@ const transporter = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 587,
   secure: false, // true for 465, false for 587
+  family: 4, // Force IPv4
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
   tls: {
-    // This helps if the environment has IPv6 issues
-    ciphers: 'SSLv3',
     rejectUnauthorized: false
   }
 });
